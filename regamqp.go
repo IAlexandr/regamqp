985af518dfd4998aa1e57f6938f411c18e4314c0
Alexandr Ivanov <aivanov@informatica.ru>
2017-02-01 11:06:30 +0300

    fix bind q to ex

--- regamqp.go
+++ regamqp.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-var ch *amqp.Channel
+var CH *amqp.Channel
 
 func FailOnError(err error, msg string) {
 	if err != nil {
@@ -19,25 +19,41 @@ func AmqpConnect(url string) (*amqp.Connection, *amqp.Channel) {
 	conn, err := amqp.Dial(amqpUrl)
 	FailOnError(err, "Failed to connect to RabbitMQ")
 	//defer conn.Close()
-	ch, err = conn.Channel()
+	CH, err = conn.Channel()
 	FailOnError(err, "Failed to open a channel")
 	//defer ch.Close()
-	log.Printf("Channel created.")
-	return conn, ch
+	log.Println("Channel created.")
+	return conn, CH
 }
 
 func ConsumeByRegId(regId string, receiveFunc func(amqp.Delivery)) {
-	q, err := ch.QueueDeclare(
-		regId, // name
+	err := CH.ExchangeDeclare(
+		"regs",   // name
+		"direct", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	FailOnError(err, "Failed to declare an exchange")
+	q, err := CH.QueueDeclare(
+		"regs." + regId, // name
 		false,   // durable
 		false,   // delete when usused
-		false,   // exclusive
+		true,   // exclusive
 		false,   // no-wait
 		nil,     // arguments
 	)
 	FailOnError(err, "Failed to declare a queue")
-
-	msgs, err := ch.Consume(
+	err = CH.QueueBind(
+		q.Name, // queue name
+		"regs." + regId,     // routing key
+		"regs", // exchange
+		false,
+		nil)
+	FailOnError(err, "Failed to bind a queue")
+	msgs, err := CH.Consume(
 		q.Name, // queue
 		"",     // consumer
 		true,   // auto-ack
@@ -56,4 +72,18 @@ func ConsumeByRegId(regId string, receiveFunc func(amqp.Delivery)) {
 	}()
 
 	log.Printf(" [*] (%q) Waiting for messages. To exit press CTRL+C", regId)
+}
+
+func PublishTo(exchangeName, route string, message string) error {
+	err := CH.Publish(
+		exchangeName,     // exchange
+		route, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+	log.Printf(" [x] Sent %s", message)
+	return err
 }
